719ef90dbb5f6a4a458eb6b2dd91084adb5a7309
GitHub Action <action@github.com>
2021-07-18 16:41:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/artdoncenter/tempplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/artdoncenter/tempplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.artdoncenter.tempplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.artdoncenter.tempplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/artdoncenter/tempplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/artdoncenter/tempplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.artdoncenter.tempplugin.services
 
+import com.github.artdoncenter.tempplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
